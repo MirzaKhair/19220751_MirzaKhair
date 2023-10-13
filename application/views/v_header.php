74972c4121152bd3374f17b44752e27413680b39
MirzaKhair <khairmirza88@gmail.com>
2023-10-13 16:21:47 +0700

    menambah file baru

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -2,14 +2,14 @@
 <html>
 
 <head>
-    <meta charset=”utf-8”>
+    <meta charset="utf-8">
     <title>Web Prog II | Merancang Template sederhana dengan
         codeigniter</title>
-    <link rel=”stylesheet” type=”text/css” href="http://localhost/pustaka-booking/assets/css/stylebuku.css"
+    <link rel="stylesheet" type="text/css" href="http://localhost/pustaka-booking/assets/css/stylebuku.css">
 </head>
 
 <body>
-    <div id=”wrapper”>
+    <div id="wrapper">
         <header>
             <hgroup>
                 <h1>RentalBuku.net</h1>
@@ -19,10 +19,10 @@
             <nav>
                 <ul>
                     <li><a href=”<?php echo
-                                    base_url() . ’index . php / web’ ?>”>Home</a></li>
+                                    base_url().'index.php/web'?>”>Home</a></li>
                     <li><a href=”<?php echo
-                                    base_url() . ’index . php / web / about’ ?>”>About</a></li>
+                                    base_url().'index.php/web/about'?>”>About</a></li>
                 </ul>
             </nav>
-            <div class=”clear”></div>
+            <div class="clear"></div>
         </header>
